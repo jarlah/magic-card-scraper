d6836ea459490d7a1ec17a4842bedabb8672bd39
Jarl André Hübenthal <jarl.andre@joint.no>
2014-05-06 22:34:00 +0200

    Making the project supported by Lighttable

--- project.clj
+++ project.clj
@@ -8,12 +8,17 @@
                  ;;JDBC deps
                  [org.clojure/java.jdbc "0.2.3"]
                  [org.xerial/sqlite-jdbc "3.7.2"]
-                 [enlive "1.1.5"]]
+                 ;;Enlive dep
+                 [enlive "1.1.5"]
+                 ;;Make sure this shit works with Lighttable
+                 [org.clojure/clojurescript "0.0-2030"]
+                 [lein-light-nrepl "0.0.13"]]
+  :repl-options {:nrepl-middleware [lighttable.nrepl.handler/lighttable-ops]}
+  :aot :all
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler magicscraper.handler/app
          :init magicscraper.handler/init
          :destroy magicscraper.handler/destroy}
-  :aot :all
   :profiles
   {:production
    {:ring
--- src/magicscraper/models/fetcher.clj
+++ src/magicscraper/models/fetcher.clj
@@ -15,26 +15,26 @@
 (def artistSelector [:td.artist html/text])
 (def setDropdownSelector [(html/attr-ends :id "setAddText" :name "setAddText") :option html/text])
 
-(defn fetch-url "Fetches from baseurl" 
+(defn fetch-url "Fetches from baseurl"
 	[url]
 	(html/html-resource (java.net.URL. (str baseurl url))))
 
-(defn extract-value "Returns the first value in a vector from a html node selection" 
+(defn extract-value "Returns the first value in a vector from a html node selection"
 	[node selector]
 	(first (html/select node selector)))
 
 ;; TODO this has to be possible to achieve with enlive
 ;; Currently just calls extract-value and gets href from attributes
 ;; then it splits the href value on "=" and takes the last argument which is multiverseid
-(defn extract-id-hack "Silly hack to achieve something that could have been done with enlive" 
+(defn extract-id-hack "Silly hack to achieve something that could have been done with enlive"
 	[cardItem]
 	(last (clojure.string/split ((comp :href :attrs)(extract-value cardItem linkSelector)) #"=")))
 
 ;; Did i mention that creating maps in Clojure is pretty ugly? Maybe its me that is not used it.
-(defn extract-card "Creates a map of the extracted attributes of a provided magic card" 
+(defn extract-card "Creates a map of the extracted attributes of a provided magic card"
 	[cardItem]
-	{:name 		(extract-value cardItem nameSelector) 
-	 :color 	(extract-value cardItem colorSelector) 
+	{:name 		(extract-value cardItem nameSelector)
+	 :color 	(extract-value cardItem colorSelector)
 	 :rarity 	(extract-value cardItem raritySelector)
 	 :set 		(extract-value cardItem setSelector)
 	 :id 		(extract-id-hack cardItem)
@@ -43,10 +43,10 @@
 (defn get-url-for-query [set]
 	(str searchurl (clojure.string/replace queryFormat #"%SET%" (clojure.string/replace set #"\s" "+"))))
 
-(defn select-sets [] 
+(defn select-sets []
 	(html/select (fetch-url "") setDropdownSelector))
 
-(defn select-cards "Simpply fetches the search result and selects the card" 
-	[set] 
+(defn select-cards "Simpply fetches the search result and selects the card"
+	[set]
 	(pmap extract-card (html/select (fetch-url (get-url-for-query set)) [:table.checklist :tr.cardItem])))
 
